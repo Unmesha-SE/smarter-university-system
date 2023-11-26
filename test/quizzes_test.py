a6d7bee76880680a39f3e140f0fc4138d6a0fe1a
Maria Vilanova <mvilanov@umd.edu>
2023-11-25 19:57:51 -0500

    Implemented Quizzes test case function #1

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -10,10 +10,13 @@ class QuizzesTest(unittest.TestCase):
         
     def test_expose_failure_01(self):
         """
-        Implement this function and two more that
-        execute the code and make it fail.
+        This test attempts to add a quiz with multiple titles (list input).
+        The program crashes in file 'quizzes_controller.py', line 63 (add_quiz).
         """
-        self.assertTrue(True, 'Example assertion.')
+        self.ctrl.clear_data()
+        quiz_id = self.ctrl.add_quiz(["Quiz #1", "Quiz #2"], "This is the first quiz.", 2023-11-24, 2023-11-25)
+        quiz = self.ctrl.get_quiz_by_id(quiz_id)
+        self.assertIsNone(quiz, 'Expecting no Quiz to be returned.')
         
 
 if __name__ == '__main__':
